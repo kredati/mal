9eed73b5351ac356e2967d2a0d644610a6dc8770
Scott Richmond <s.richmond@utoronto.ca>
2020-08-30 00:44:03 -0400

    Copy step 3 over (no changes to this file tho)

---
+++ impls/paidia/step3_env.js
@@ -0,0 +1,40 @@
+import * as reader from './reader.js';
+import * as printer from './printer.js';
+import * as evaluate from './eval.js';
+import * as env from './env.js'
+import {pipe_some} from './util.js';
+
+let stdin = Deno.iter(Deno.stdin);
+
+let dec = new TextDecoder();
+let enc = new TextEncoder();
+
+let read_line = (chunk) => {
+  return dec.decode(chunk);
+};
+
+let print_out = (string) => {
+  Deno.stdout.write(enc.encode(string + '\n'));
+};
+
+let prompt = () => {
+  Deno.stdout.write(enc.encode("user> "));
+};
+
+let loop = pipe_some(
+  read_line,
+  reader.read,
+  (ast) => evaluate.evaluate(ast, env.base),
+  printer.print,
+  print_out
+);
+
+///// do the thing
+
+console.log('Welcome to Paidia!');
+prompt();
+
+for await (let chunk of stdin) {
+  loop(chunk);
+  prompt();
+};
