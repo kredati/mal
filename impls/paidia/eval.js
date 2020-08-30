84789605de10017e9a5fb4b2879b35eeb0617671
Scott Richmond <s.richmond@utoronto.ca>
2020-08-30 00:43:25 -0400

    Add `let*` and `def!` special forms

--- impls/paidia/eval.js
+++ impls/paidia/eval.js
@@ -1,16 +1,14 @@
 import * as t from './types.js';
-import * as r from './reader.js';
-import {env} from './repl_env.js';
-import {get} from './util.js';
+import {Env} from './env.js';
+import {get, chunk} from './util.js';
 
 let evaluate = (ast, env) => {
-  ast
   if (ast.length === 0) return undefined;
   try {  return ast
     .map(value => inner_eval(value, env))
     .map(lift);
   } catch (e) {
-    return [t.PError.create(`.*${e.name}: ${e.message}.*`)];
+    return [t.PError.create(`${e.name}: ${e.message}`)];
   }
 };
 
@@ -19,13 +17,21 @@ let inner_eval = (value, env) => {
     case t.PName[t.type]: {
       let name = value.value;
       let lookup = get(name, env);
-      if (lookup == null) throw Error(`Unbound identifier: ${name}`);
+      if (lookup == null) throw Error(`'${name}' not found`);
       return lookup;
     }
     case t.PList[t.type]: {
       if (value.value.length === 0) return value;
-      let [fn, ...args] = value.value.map(v => inner_eval(v, env));
-      return fn(...args);
+      let [first, ...rest] = value.value;
+      let fn = inner_eval(first, env);
+      switch(fn.name) {
+        case 'let*': return eval_let(rest, env);
+        case 'def!': return eval_def(rest, env);
+        default: {
+          let args = rest.map(v => inner_eval(v, env));
+          return fn(...args);
+        };
+      }
     }
     case t.PNumber[t.type]:
     case t.PBoolean[t.type]:
@@ -33,6 +39,33 @@ let inner_eval = (value, env) => {
       return value.value;
     }
   }
+};
+
+let eval_let = ([raw_bindings, ...exprs], env) => {
+  if (raw_bindings[t.type] !== t.PList[t.type])
+    throw Error(`let* bindings must be a list; you gave me ${raw_bindings.show()}::${raw_bindings[t.type].description}.`);
+  raw_bindings = raw_bindings.value;
+  if (raw_bindings.length % 2 !== 0)
+    throw Error('let* bindings must have an even number of entries.');
+  let bindings = chunk(raw_bindings);
+  let let_env = Env.create(env, {});
+  for (let [raw_name, raw_value] of bindings) {
+    if (raw_name[t.type] !== t.PName[t.type])
+      throw Error(`I can only bind values to valid names.`);
+    let name = raw_name.value;
+    let value = inner_eval(raw_value, let_env);
+    Env.set(let_env, name, value);
+  }
+  let evaled = exprs.map((expr) => inner_eval(expr, let_env));
+  return evaled[evaled.length - 1];
+};
+
+let eval_def = ([name, expr], env) => {
+  if (name[t.type] !== t.PName[t.type])
+    throw Error('I can only bind names');
+  let value = inner_eval(expr, env);
+  Env.set(env, name.value, value);
+  return value;
 };
 
 let lift = (value) => {
